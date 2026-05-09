9a87a7a9dec396df9d55389e3c040a918e2c368c
kenform <41068154+kenform@users.noreply.github.com>
2026-05-09 04:46:06 +0300

    Restore original sneaker photos

--- src/utils/imagePath.js
+++ src/utils/imagePath.js
@@ -1,8 +1,21 @@
+const SNEAKER_IMAGES = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg"
+]
+
 export const getSneakerImageUrl = (id = 1) => {
   const numericId = Number(id)
-  const safeId = Number.isFinite(numericId) && numericId > 0 ? ((numericId - 1) % 12) + 1 : 1
+  const safeIndex = Number.isFinite(numericId) && numericId > 0 ? (numericId - 1) % SNEAKER_IMAGES.length : 0
 
-  return `img/sneakers/sneaker-${safeId}.svg`
+  return `img/sneakers/${SNEAKER_IMAGES[safeIndex]}`
 }
 
 export const getPublicImageUrl = (imageUrl, id = 1) => {
